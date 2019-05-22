91f958b80cd39d9232229c5bfc2bf9fd987b9a8b
Nandoedwin007 <cor14148@uvg.edu.gt>
2019-05-22 17:05:08 -0600

    Versión Pre Mapa Playa

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using ChrisTutorials.Persistent;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.PlayLoop(backgroundMusic, transform, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
