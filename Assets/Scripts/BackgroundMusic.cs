9a0ef86d5cb41688162b3b8504bcc9a7a0dbc190
Nandoedwin007 <cor14148@uvg.edu.gt>
2019-05-22 21:52:32 -0600

    V3 Mejoras al Mapa e inclusión Mobs

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -9,7 +9,7 @@ public class BackgroundMusic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.PlayLoop(backgroundMusic, transform, 0.5f);
+        //AudioManager.Instance.PlayLoop(backgroundMusic, transform, 0.5f);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Map_Obj/Rueda_Script.cs
+++ Assets/Scripts/Map_Obj/Rueda_Script.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Rueda_Script : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float force = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb)
+        {
+            if(rb.velocity.x > 0)
+            {
+                rb.AddForce(force,0,0,ForceMode.Force);
+            }
+            if (rb.velocity.x < 0)
+            {
+                rb.AddForce(-force, 0, 0, ForceMode.Force);
+            }
+            //rb.AddForce(Input.GetAxis("Vertical") * force, 0, Input.GetAxis("Horizontal") * force);
+        }
     }
 }
